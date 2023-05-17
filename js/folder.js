7fec25ac51d4cdcd0a3196993f9d870b7dfeb159
kevin <orgeron.kevin@gmail.com>
2023-05-16 22:44:09 -0500

    working on folder animation

--- js/folder.js
+++ js/folder.js
@@ -1,7 +1,33 @@
 const folder = document.querySelector('.folder');
+const folder_back = document.querySelector('.folder__back');
+const folder_front = document.querySelector('.folder__front');
+const texts = document.querySelectorAll('.unselectable')
+let isOff = false;
+
+function turnOff () {
+    folder.style.animationIterationCount = '1';
+    return isOff = true;
+}
+function off (statement) {
+let checked = false;
+    if (statement == true) {
+        folder_front.style.animation = 'folder-open 1.5s ease-in-out forwards';
+         texts.forEach(text => {
+            text.style.animation = 'hide-text 1s forwards';
+         })
+    }
+
+
+
+
+}
 
 folder.addEventListener('click', ()=> {
+
+    let itsOff = turnOff();
+    off(itsOff);
 
 
 
 })
+
